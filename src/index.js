1f5dcd1f2904ff7c5f7fec1599321374f13237a0
Lauri <siilike@ruja.ee>
2020-12-06 16:58:01 +0200

    initial comimt

---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+const editor = require('@toast-ui/react-image-editor')
+
+console.log(editor)
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	optimization: {
+		minimize: false,
+	},
+}
+
+module.exports = config
